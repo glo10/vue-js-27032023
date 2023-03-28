a50512dc9cc3d2d46f0f57034f1ad8c04d5a3d56
Glodie Tshimini <contact@tshimini.fr>
2023-03-28 09:14:12 +0200

    refactor: correction of exercise 4bis(split components + this.$refs)

---
+++ 0-exercices/corrections/exercice4/src/components/SelectCurrency.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div class="input-group mb-3">
+      <div class="input-group-prepend">
+        <span class="input-group-text">{{ label }}</span>
+      </div>
+      <select class="form-control from" @change="choiceCurrency($event, label)">
+        <option value="">Select One …</option>
+        <option v-for='cr in currencies' :value="cr" :key="cr">{{ cr }} </option>
+      </select>
+    </div>
+  </div>
+</template>
+<script>
+import {currencies} from '../assets/currencies.json'
+export default {
+  created() {
+    this.currencies = currencies
+  },
+  mounted() {
+    console.log('mounted', this)
+  },
+  updated() {
+    console.log('updated', this)
+  },
+  unmounted() {
+    console.log('unmounted', this)
+  },
+  emits: ['choice-currency'],
+  props: {
+    label: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      currencies:null
+    }
+  },
+  methods: {
+    choiceCurrency(evt, origin) {
+      this.$emit(`choice-currency`,{ origin: origin, value: evt.target.value} )
+    }
+  },
+}
+</script>
---
+++ 0-exercices/corrections/exercice4/src/views/ConverterPage.vue
@@ -0,0 +1,109 @@
+<template>
+  <div>
+    <TheHeader :title="headerTitle" />
+    <main class="container">
+      <div class="main">
+        <TheResult v-show="isSwhoResult" ref="result" />
+        <TheAmountForm ref="inputAmount"/>
+        <SelectCurrency 
+          v-for="label in labels"
+          :label="label"
+          :key="label" @choice-currency="selectedCurrency($event)"
+        />
+        <ConverterBtn
+          v-for="btn in btns"
+          :key="btn.id"
+          :btn="btn"
+          v-on="btn.id === 1 ? {'get-result': getResults}:{'clear':clearValues}"
+        />
+      </div>
+      <footer>
+        <TheCredit />
+      </footer>
+    </main>
+  </div>
+</template>
+<script>
+import TheHeader from '@/components/TheHeader.vue'
+import TheAmountForm from '../components/TheAmountForm.vue'
+import SelectCurrency from '@/components/SelectCurrency.vue'
+import TheResult from '../components/TheResult.vue'
+import TheCredit from '../components/TheCredit.vue'
+import ConverterBtn from '../components/ConverterBtn.vue'
+
+export default {
+  components: {
+    TheHeader,
+    TheAmountForm,
+    SelectCurrency,
+    TheResult,
+    TheCredit,
+    ConverterBtn
+},
+  data() {
+    return {
+      btns: [
+        {
+          id: 1,
+          customEvent: 'get-result',
+          label: 'convert'
+        },
+        {
+          id:2,
+          customEvent: 'clear',
+          label: 'reset'
+        },
+      ],
+      labels: {
+        from: 'from',
+        to: 'to'
+      },
+      headerTitle: 'Currency Converter',
+      rateFrom: null,
+      rateTo: null,
+      isSwhoResult: false
+    }
+  },
+  methods: {
+    selectedCurrency(payload) {
+      console.log('parent', payload)
+      if(payload.origin === 'from') {
+        this.rateFrom = payload.value
+      } else if(payload.origin === 'to') {
+        this.rateTo = payload.value
+      }
+    },
+    getResults() {
+      console.log('this', this.$refs)
+      let currency = localStorage.getItem('currency')
+      if (currency === null) {
+        fetch('https://open.er-api.com/v6/latest/USD')
+          .then(currency => {
+            return currency.json()
+          })
+          .then((currency) => {
+            localStorage.setItem('currency', JSON.stringify(currency))
+            this.displayResults(currency.rates)
+          })
+      } else {
+        currency = JSON.parse(localStorage.getItem('currency'))
+        this.displayResults(currency.rates)
+      }
+    },
+    displayResults(rates) {
+      if(this.from === null || this.to === null) {
+        return 
+      }
+      let fromRate = rates[this.rateFrom]
+      let toRate = rates[this.rateTo]
+      this.$refs.result.final = ((toRate / fromRate) * this.$refs.inputAmount.amount).toFixed(2)
+      this.isSwhoResult = true
+    },
+    clearValues() {
+      this.$refs.inputAmount.amount = 0
+      this.$refs.result.final = 0
+      this.isSwhoResult = false
+    }
+  }
+}
+</script>
